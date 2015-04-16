808f681213db28b0fd9bd2ef45d87a32d71f6e3c
spockwangs <wbbtiger@gmail.com>
2015-04-16 12:57:25 +0800

    Fixed minor bugs.

--- emacs.d/lisp/init-preload-local.el
+++ emacs.d/lisp/init-preload-local.el
@@ -1,5 +1,30 @@
-;; Customizations before any other configurations.
+;;; init-preload-local.el --- Local preload customizations.  -*- lexical-binding: t; -*-
 
-;(setq url-proxy-services '(("http" . "proxy.tencent.com:8080")))
+;; Copyright (C) 2015  
+
+;; Author:  <spockwang@SPOCKWANG-PC0>
+;; Keywords: local
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Local preload customizations.
+
+;;; Code:
+
+(setq url-proxy-services '(("http" . "proxy.tencent.com:8080")))
 
 (provide 'init-preload-local)
+;;; init-preload-local.el ends here
